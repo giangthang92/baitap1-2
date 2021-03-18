a58d5a4aa6124f19d14965c0c6d96a395132ba14
giangthang <shikaku04021992@gmail.com>
2021-03-18 20:21:42 +0700

    9-10

--- index.js
+++ index.js
@@ -13,10 +13,44 @@ app.use('/public', express.static('./public'));
 app.engine('ejs', engine);
 
 app.get('/', (req, res) => {
-    res.render('client/page/index');
+    const baiviettao =  [
+          { title : 'adaSSSSSSSSSSSSSS',
+            description : ' day la mo ta moi',
+            author: 'GiangThang92', 
+            createdAt: '18 - 03 -2021',
+          },
+          {
+            title : 'ABBBBBBBBBBBBBBBBBBB',
+            description : ' day la mo ta moi 2',
+            author: 'GiangThang92', 
+            createdAt: '18 - 03 -2021',
+          },
+          {
+            title : 'ACCCCCCCCCCCCCCCCC',
+            description : ' day la mo ta moi 3',
+            author: 'GiangThang92', 
+            createdAt: '18 - 03 -2021',
+          },
+          {
+            title : 'ADDDDDDDDDDDDDDDDDDD',
+            description : ' day la mo ta moi 4',
+            author: 'GiangThang92', 
+            createdAt: '18 - 03 -2021',
+          }
+    ]
+    res.render('client/page/index', {
+        posts : baiviettao,
+    });
 });
 app.get('/post',(req, res) => {
-    res.render('client/page/post');
+    const baiviet = {   title: 'GiangAAAAAA', 
+                        description: ' Bai viet 2000 tu', 
+                        author: 'GiangThang', 
+                        createAt: '18/03/2021',
+                        link: 'http://24h.com.vn/' };
+    res.render('client/page/post', {
+        post : baiviet, 
+    });
 });
 app.listen(port,()=>{
     console.log('Run');
